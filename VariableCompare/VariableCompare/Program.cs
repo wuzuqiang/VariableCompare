9782f30048dabb757bdada1b663c75e4aa9101cb
wuzuqiang <37428362+wuzuqiang@users.noreply.github.com>
2018-10-17 10:40:15 +0800

    暂存

--- VariableCompare/VariableCompare/Program.cs
+++ VariableCompare/VariableCompare/Program.cs
@@ -10,10 +10,16 @@ namespace VariableCompare
     {
         static void Main(string[] args)
         {
+            CharCompareString();
+            Console.ReadLine();
         }
 
-        private void CharCompareString()
+        private static void CharCompareString()
         {
+            string strReciveData = "/12345678901234567890>";
+            Console.WriteLine("{0}和{1}的strReciveData[0]比较结果为{2}", '/', strReciveData, '/' == strReciveData[0]);
+            Console.WriteLine("字符串{0}和{1}的strReciveData[0]比较结果为{2}", "/", strReciveData, '/' == strReciveData[0]);
+            Console.WriteLine("{0}和{1}的strReciveData[22-1]比较结果为{2}", '>', strReciveData, '>' == strReciveData[22 - 1]);
         }
     }
 }
